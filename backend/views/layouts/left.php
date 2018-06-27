79eca258f86225da1183b21560eadedc49601d34
pzzrudlf <pzzrudlf@localhost.localdomain>
2018-06-28 01:50:35 +0800

    M

--- backend/views/layouts/left.php
+++ backend/views/layouts/left.php
@@ -14,40 +14,22 @@ use dmstr\widgets\Menu;
 //            'items' => MenuHelper::getAssignedMenu(Yii::$app->user->id),
             'items' => [
                 [
-                    'label' => '管理员管理',
+                    'label' => '权限管理',
                     'icon' => 'circle-o',
                     'url' => '#',
                     'items' => [
                         ['label' => '路由管理', 'icon' => 'circle-o', 'url' => ['/rbac/route'],],
-                        ['label' => '权限分配', 'icon' => 'circle-o', 'url' => ['/rbac/permissions'],],
-                        [
-                            'label' => 'Level One',
-                            'icon' => 'circle-o',
-                            'url' => '#',
-                            'items' => [
-                                ['label' => 'Level Two', 'icon' => 'circle-o', 'url' => '#',],
-                                [
-                                    'label' => 'Level Two',
-                                    'icon' => 'circle-o',
-                                    'url' => '#',
-                                    'items' => [
-                                        ['label' => 'Level Three', 'icon' => 'circle-o', 'url' => '#',],
-                                        ['label' => 'Level Three', 'icon' => 'circle-o', 'url' => '#',],
-                                    ],
-                                ],
-                            ],
-                        ],
+                        ['label' => '权限管理', 'icon' => 'circle-o', 'url' => ['/rbac/permission'],],
+                        ['label' => '角色分配', 'icon' => 'circle-o', 'url' => ['/rbac/role'],],
+                        ['label' => '管理员列表', 'icon' => 'circle-o', 'url' => ['/admin/index'],],
                     ],
                 ],
                 [
-                    'label' => '文章管理',
+                    'label' => '会员管理',
                     'icon' => 'circle-o',
                     'url' => '#',
                     'items' => [
-                        ['label' => '文章分类', 'icon' => 'circle-o', 'url' => ['/article/article-cat'],],
-                        ['label' => '文章列表', 'icon' => 'circle-o', 'url' => ['/article/index'],],
-                        ['label' => '文章评论', 'icon' => 'circle-o', 'url' => ['/article/comment'],],
-                        ['label' => '文章评论回复', 'icon' => 'circle-o', 'url' => ['/article/reply']],
+                        ['label' => '会员列表', 'icon' => 'circle-o', 'url' => ['/user/index'],],
                     ],
                 ],
                 [
@@ -55,20 +37,19 @@ use dmstr\widgets\Menu;
                     'icon' => 'circle-o',
                     'url' => '#',
                     'items' => [
-                        ['label' => '商品分类', 'icon' => 'circle-o', 'url' => ['/goods/goods-cat'],],
+                        ['label' => '商品分类', 'icon' => 'circle-o', 'url' => ['/goods/cate'],],
+                        ['label' => '商品规格', 'icon' => 'circle-o', 'url' => ['/goods/spec']],
                         ['label' => '商品列表', 'icon' => 'circle-o', 'url' => ['/goods/index'],],
                         ['label' => '商品评论', 'icon' => 'circle-o', 'url' => ['/goods/comment'],],
                         ['label' => '商品咨询', 'icon' => 'circle-o', 'url' => ['/goods/consult']],
                     ],
                 ],
                 [
-                    'label' => '日志管理',
+                    'label' => '订单管理',
                     'icon' => 'circle-o',
                     'url' => '#',
                     'items' => [
-                        ['label' => '用户登录日志', 'icon' => 'circle-o', 'url' => ['/log/user-login'],],
-                        ['label' => '管理员登录日志', 'icon' => 'circle-o', 'url' => ['/log/admin-login'],],
-                        ['label' => '管理员操作日志', 'icon' => 'circle-o', 'url' => ['/log/admin-operation']]
+                        ['label' => '订单列表', 'icon' => 'circle-o', 'url' => ['/order/index'],],
                     ],
                 ],
                 [
