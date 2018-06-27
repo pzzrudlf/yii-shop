324f45bb7590933ab22fc4e9c2da676547948811
pzzrudlf <pzzrudlf@google.com>
2018-06-27 20:52:09 +0800

    M

---
+++ doc/d.sql
@@ -0,0 +1,221 @@
+﻿DROP TABLE IF EXISTS `yii_article_comment`;
+CREATE TABLE `yii_article_comment` (
+  `id` int(11) NOT NULL DEFAULT '0',
+  `article_id` int(11) NOT NULL DEFAULT '0' COMMENT '文章id',
+  `article_type` varchar(255) DEFAULT NULL COMMENT '文章类型',
+  `content` varchar(255) DEFAULT NULL COMMENT '评论内容',
+  `from_uid` int(11) DEFAULT NULL COMMENT '评论人的id',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='文章评论表';
+
+DROP TABLE IF EXISTS `yii_article_comment_reply`;
+CREATE TABLE `yii_article_comment_reply` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `comment_id` varchar(255) DEFAULT NULL COMMENT '评论id',
+  `reply_id` varchar(255) DEFAULT NULL COMMENT '回复目标id',
+  `reply_type` varchar(255) DEFAULT NULL COMMENT '回复类型',
+  `content` varchar(255) DEFAULT NULL COMMENT '回复内容',
+  `from_uid` varchar(255) DEFAULT NULL COMMENT '回复人id',
+  `to_uid` varchar(255) DEFAULT NULL COMMENT '评论人id',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='评论的回复表';
+
+DROP TABLE IF EXISTS `yii_brand`;
+CREATE TABLE `yii_brand` (
+  `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT COMMENT '品牌表',
+  `name` varchar(60) NOT NULL DEFAULT '' COMMENT '品牌名称',
+  `logo` varchar(80) NOT NULL DEFAULT '' COMMENT '品牌logo',
+  `desc` text NOT NULL COMMENT '品牌描述',
+  `url` varchar(255) NOT NULL DEFAULT '' COMMENT '品牌地址',
+  `sort` int(3) unsigned NOT NULL DEFAULT '50' COMMENT '排序',
+  `cat_name` varchar(128) DEFAULT '' COMMENT '品牌分类',
+  `parent_cat_id` int(11) DEFAULT '0' COMMENT '分类id',
+  `cat_id` int(10) DEFAULT '0' COMMENT '分类id',
+  `is_hot` tinyint(1) DEFAULT '0' COMMENT '是否推荐',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='品牌表';
+
+DROP TABLE IF EXISTS `yii_goods`;
+CREATE TABLE `yii_goods` (
+  `goods_id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT COMMENT '商品id',
+  `cat_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '分类id',
+  `extend_cat_id` int(11) DEFAULT '0' COMMENT '扩展分类id',
+  `goods_sn` varchar(60) NOT NULL DEFAULT '' COMMENT '商品编号',
+  `goods_name` varchar(120) NOT NULL DEFAULT '' COMMENT '商品名称',
+  `click_count` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '点击数',
+  `brand_id` smallint(5) unsigned NOT NULL DEFAULT '0' COMMENT '品牌id',
+  `store_count` smallint(5) unsigned NOT NULL DEFAULT '10' COMMENT '库存数量',
+  `comment_count` smallint(5) DEFAULT '0' COMMENT '商品评论数',
+  `weight` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '商品重量克为单位',
+  `volume` double(10,4) unsigned NOT NULL DEFAULT '0.0000' COMMENT '商品体积。单位立方米',
+  `market_price` decimal(10,2) unsigned NOT NULL DEFAULT '0.00' COMMENT '市场价',
+  `shop_price` decimal(10,2) unsigned NOT NULL DEFAULT '0.00' COMMENT '本店价',
+  `cost_price` decimal(10,2) DEFAULT '0.00' COMMENT '商品成本价',
+  `price_ladder` text COMMENT '价格阶梯',
+  `keywords` varchar(255) NOT NULL DEFAULT '' COMMENT '商品关键词',
+  `goods_remark` varchar(255) NOT NULL DEFAULT '' COMMENT '商品简单描述',
+  `goods_content` text COMMENT '商品详细描述',
+  `mobile_content` text COMMENT '手机端商品详情',
+  `original_img` varchar(255) NOT NULL DEFAULT '' COMMENT '商品上传原始图',
+  `is_virtual` tinyint(3) unsigned NOT NULL DEFAULT '0' COMMENT '是否为虚拟商品 1是，0否',
+  `virtual_indate` int(11) DEFAULT '0' COMMENT '虚拟商品有效期',
+  `virtual_limit` smallint(6) DEFAULT '0' COMMENT '虚拟商品购买上限',
+  `virtual_refund` tinyint(1) DEFAULT '1' COMMENT '是否允许过期退款， 1是，0否',
+  `is_on_sale` tinyint(1) unsigned NOT NULL DEFAULT '1' COMMENT '是否上架',
+  `is_free_shipping` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '是否包邮0否1是',
+  `on_time` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '商品上架时间',
+  `sort` smallint(4) unsigned NOT NULL DEFAULT '50' COMMENT '商品排序',
+  `is_recommend` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '是否推荐',
+  `is_new` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '是否新品',
+  `is_hot` tinyint(1) DEFAULT '0' COMMENT '是否热卖',
+  `last_update` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '最后更新时间',
+  `goods_type` smallint(5) unsigned NOT NULL DEFAULT '0' COMMENT '商品所属类型id，取值表goods_type的cat_id',
+  `spec_type` smallint(5) DEFAULT '0' COMMENT '商品规格类型，取值表goods_type的cat_id',
+  `give_integral` mediumint(8) unsigned NOT NULL DEFAULT '0' COMMENT '购买商品赠送积分',
+  `exchange_integral` int(10) NOT NULL DEFAULT '0' COMMENT '积分兑换：0不参与积分兑换，积分和现金的兑换比例见后台配置',
+  `suppliers_id` smallint(5) unsigned NOT NULL DEFAULT '0' COMMENT '供货商ID',
+  `sales_sum` int(11) DEFAULT '0' COMMENT '商品销量',
+  `prom_type` tinyint(1) DEFAULT '0' COMMENT '0默认1抢购2团购3优惠促销4预售5虚拟(5其实没用)6拼团',
+  `prom_id` int(11) NOT NULL DEFAULT '0' COMMENT '优惠活动id',
+  `commission` decimal(10,2) DEFAULT '0.00' COMMENT '佣金用于分销分成',
+  `spu` varchar(128) DEFAULT '' COMMENT 'SPU',
+  `sku` varchar(128) DEFAULT '' COMMENT 'SKU',
+  `template_id` int(11) unsigned DEFAULT '0' COMMENT '运费模板ID',
+  `video` varchar(255) DEFAULT '' COMMENT '视频',
+  PRIMARY KEY (`goods_id`),
+  KEY `goods_sn` (`goods_sn`),
+  KEY `cat_id` (`cat_id`),
+  KEY `last_update` (`last_update`),
+  KEY `brand_id` (`brand_id`),
+  KEY `goods_number` (`store_count`),
+  KEY `goods_weight` (`weight`),
+  KEY `sort_order` (`sort`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='商品表';
+
+DROP TABLE IF EXISTS `yii_goods_attr_by_spu`;
+CREATE TABLE `yii_goods_attr_by_spu` (
+  `goods_attr_id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '商品属性id自增',
+  `goods_id` mediumint(8) unsigned NOT NULL DEFAULT '0' COMMENT '商品id',
+  `attr_id` smallint(5) unsigned NOT NULL DEFAULT '0' COMMENT '属性id',
+  `attr_value` text NOT NULL COMMENT '属性值',
+  `attr_price` varchar(255) NOT NULL DEFAULT '' COMMENT '属性价格',
+  PRIMARY KEY (`goods_attr_id`),
+  KEY `goods_id` (`goods_id`),
+  KEY `attr_id` (`attr_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='商品属性表(跟商品模型属性表关联)';
+
+DROP TABLE IF EXISTS `yii_goods_attribute`;
+CREATE TABLE `yii_goods_attribute` (
+  `attr_id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '属性id',
+  `attr_name` varchar(60) NOT NULL DEFAULT '' COMMENT '属性名称',
+  `type_id` smallint(5) unsigned NOT NULL DEFAULT '0' COMMENT '属性分类id',
+  `attr_index` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '0不需要检索 1关键字检索 2范围检索',
+  `attr_type` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '0唯一属性 1单选属性 2复选属性',
+  `attr_input_type` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT ' 0 手工录入 1从列表中选择 2多行文本框',
+  `attr_values` text NOT NULL COMMENT '可选值列表',
+  `order` tinyint(3) unsigned NOT NULL DEFAULT '50' COMMENT '属性排序',
+  PRIMARY KEY (`attr_id`),
+  KEY `cat_id` (`type_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='商品模型属性表(生产日期等)';
+
+DROP TABLE IF EXISTS `yii_goods_category`;
+CREATE TABLE `yii_goods_category` (
+  `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT COMMENT '商品分类id',
+  `name` varchar(90) NOT NULL DEFAULT '' COMMENT '商品分类名称',
+  `mobile_name` varchar(64) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT '' COMMENT '手机端显示的商品分类名',
+  `parent_id` smallint(5) unsigned NOT NULL DEFAULT '0' COMMENT '父id',
+  `parent_id_path` varchar(128) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT '' COMMENT '家族图谱',
+  `level` tinyint(1) DEFAULT '0' COMMENT '等级',
+  `sort_order` tinyint(1) unsigned NOT NULL DEFAULT '50' COMMENT '顺序排序',
+  `is_show` tinyint(1) unsigned NOT NULL DEFAULT '1' COMMENT '是否显示',
+  `image` varchar(512) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT '' COMMENT '分类图片',
+  `is_hot` tinyint(1) DEFAULT '0' COMMENT '是否推荐为热门分类',
+  `cat_group` tinyint(1) DEFAULT '0' COMMENT '分类分组默认0',
+  `commission_rate` tinyint(1) DEFAULT '0' COMMENT '分佣比例',
+  PRIMARY KEY (`id`),
+  KEY `parent_id` (`parent_id`) USING BTREE
+) ENGINE=MyISAM AUTO_INCREMENT=848 DEFAULT CHARSET=utf8 COMMENT='商品分类表';
+
+DROP TABLE IF EXISTS `yii_goods_comment`;
+CREATE TABLE `yii_goods_comment` (
+  `comment_id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '评论id',
+  `goods_id` mediumint(8) unsigned NOT NULL DEFAULT '0' COMMENT '商品id',
+  `email` varchar(60) NOT NULL DEFAULT '' COMMENT 'email邮箱',
+  `username` varchar(60) NOT NULL DEFAULT '' COMMENT '用户名',
+  `content` text NOT NULL COMMENT '评论内容',
+  `add_time` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '添加时间',
+  `ip_address` varchar(15) NOT NULL DEFAULT '' COMMENT 'ip地址',
+  `is_show` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '是否显示',
+  `parent_id` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '父id',
+  `user_id` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '评论用户',
+  `img` text COMMENT '晒单图片',
+  `order_id` mediumint(8) DEFAULT '0' COMMENT '订单id',
+  `deliver_rank` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '物流评价等级',
+  `goods_rank` tinyint(1) DEFAULT '0' COMMENT '商品评价等级',
+  `service_rank` tinyint(1) DEFAULT '0' COMMENT '商家服务态度评价等级',
+  `zan_num` int(10) NOT NULL DEFAULT '0' COMMENT '被赞数',
+  `zan_userid` varchar(255) NOT NULL DEFAULT '' COMMENT '点赞用户id',
+  `is_anonymous` tinyint(1) NOT NULL DEFAULT '0' COMMENT '是否匿名评价:0不是，1是',
+  `rec_id` int(11) DEFAULT NULL COMMENT '订单商品表ID',
+  PRIMARY KEY (`comment_id`),
+  KEY `parent_id` (`parent_id`),
+  KEY `id_value` (`goods_id`),
+  KEY `order_id` (`order_id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='商品评论表';
+
+DROP TABLE IF EXISTS `yii_goods_consult`;
+CREATE TABLE `yii_goods_consult` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '商品咨询id',
+  `goods_id` int(11) DEFAULT '0' COMMENT '商品id',
+  `username` varchar(32) CHARACTER SET utf8 DEFAULT '' COMMENT '网名',
+  `add_time` int(11) DEFAULT '0' COMMENT '咨询时间',
+  `consult_type` tinyint(1) DEFAULT '1' COMMENT '1 商品咨询 2 支付咨询 3 配送 4 售后',
+  `content` varchar(1024) CHARACTER SET utf8 DEFAULT '' COMMENT '咨询内容',
+  `parent_id` int(11) DEFAULT '0' COMMENT '父id 用于管理员回复',
+  `is_show` tinyint(1) DEFAULT '0' COMMENT '是否显示',
+  `status` tinyint(1) DEFAULT '0' COMMENT '管理员回复状态，0未回复，1已回复',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='商品咨询表';
+
+DROP TABLE IF EXISTS `yii_goods_spec_detail`;
+CREATE TABLE `yii_goods_spec_detail` (
+  `item_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '商品规格id',
+  `goods_id` int(11) DEFAULT '0' COMMENT '商品id',
+  `key` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '规格键名',
+  `key_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL COMMENT '规格键名中文',
+  `price` decimal(10,2) DEFAULT NULL COMMENT '价格',
+  `store_count` int(11) unsigned DEFAULT '10' COMMENT '库存数量',
+  `bar_code` varchar(32) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT '' COMMENT '商品条形码',
+  `sku` varchar(128) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT '' COMMENT 'SKU',
+  `spec_img` varchar(255) DEFAULT NULL COMMENT '规格商品主图',
+  `prom_id` int(10) DEFAULT '0' COMMENT '活动id',
+  `prom_type` tinyint(2) DEFAULT '0' COMMENT '参加活动类型',
+  PRIMARY KEY (`item_id`),
+  KEY `key` (`key`)
+) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 COMMENT='商品sku规格表，与商品id关联，与规格表笛卡尔积';
+
+DROP TABLE IF EXISTS `yii_goods_type`;
+CREATE TABLE `yii_goods_type` (
+  `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT COMMENT 'id自增',
+  `name` varchar(60) NOT NULL DEFAULT '' COMMENT '类型名称',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COMMENT='商品模型表';
+
+DROP TABLE IF EXISTS `yii_spec`;
+CREATE TABLE `yii_spec` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '规格表',
+  `type_id` int(11) DEFAULT '0' COMMENT '规格类型',
+  `name` varchar(55) DEFAULT NULL COMMENT '规格名称',
+  `order` int(11) DEFAULT '50' COMMENT '排序',
+  `search_index` tinyint(1) DEFAULT '1' COMMENT '是否需要检索：1是，0否',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='规格表';
+
+DROP TABLE IF EXISTS `yii_spec_item`;
+CREATE TABLE `yii_spec_item` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '规格项id',
+  `spec_id` int(11) DEFAULT NULL COMMENT '规格id',
+  `item` varchar(54) DEFAULT NULL COMMENT '规格项',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COMMENT='规格项目表';
